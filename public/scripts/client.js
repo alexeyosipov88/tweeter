fd356eb9a4759e4d21ccdd44f3fdc072420ee955
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-16 16:56:16 +0000

    added hover functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  $('.tweet').hover(function(){
+    $(this).css('box-shadow', '10px 5px 5px grey')
+  }, function() {
+    $(this).css('box-shadow', 'unset')
+  });
+  $('.fas.fa-flag').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', 'unset');
+  })
 
+  $('.fas.fa-retweet').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', 'unset');
+  })
+
+  $('.fas.fa-heart').hover(function() {
+    $(this).css('color', 'orange');
+  }, function() {
+    $(this).css('color', 'unset');
+  })
+
+
+
+});
